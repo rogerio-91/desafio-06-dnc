8941b1e037c8a77e8e2d51a5198dde28dfe3a806
Rogério Júnior <rogerio.rocha91@gmail.com>
2023-07-15 19:44:57 -0300

    Adicionado variavel de ambiente,projeto finalizado!

--- db/conn.js
+++ db/conn.js
@@ -1,15 +1,15 @@
+require('dotenv').config();
 const mongoose = require('mongoose');
 
 async function main () {
     mongoose.set('strictQuery', true);
 
     try {
-        await mongoose.connect("mongodb+srv://rogerio:1dNgKSowzc8YvGvY@cluster0.lledoqo.mongodb.net/?retryWrites=true&w=majority")
+        await mongoose.connect(process.env.MONGODB_URI);
         console.log('Database connected!');
     } catch (error) {
         console.log(`Erro: ${error}`);
     }
-
 }
 
 module.exports = main;
